3c67880b06202809d606017e1ab2cb59cd733e0a
Juan José Arenas G. <juanjoarenas1218@gmail.com>
2024-10-23 08:52:56 -0500

    seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\User;
+use Database\Seeders;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -15,9 +16,7 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        //INVOCAMOS NUESTRO SEEDER PARA LLENAR LA DB
+        $this->call(UsersTableSeeders::class);
     }
 }
--- database/seeders/UsersTableSeeders.php
+++ database/seeders/UsersTableSeeders.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class UsersTableSeeders extends Seeder
 {
@@ -13,11 +14,13 @@ class UsersTableSeeders extends Seeder
      */
     public function run(): void
     {
-        for($i=0;$i<100;$i++){
+        for($i = 0;$i<100;$i++){
             User::create([
-                "names" => fake()->name,
-                "email" => fake()->email,
-                "password" => bcrypt('secret'), // Password is 'secret' for simplicity. Replace it with a stronger password.
+                'names' => fake()->name,
+                'lastnames' => fake()->lastName,
+                'email' => fake()->email,
+                'password' => Hash::make('password'),
+                'address' => fake()->address,
             ]);
         }
     }
