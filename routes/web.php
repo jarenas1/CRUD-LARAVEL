29471a7fcb05795e251010d2d427546c751fa91d
Juan José Arenas G. <juanjoarenas1218@gmail.com>
2024-10-23 09:04:19 -0500

    create routes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/usuarios', [UserController::class, 'index'])->name('usuarios.index'); //esa referencia le asigna el nombre usuarios.index a la ruta para que sea más facil de manejar
+Route::get('usuarios/{id}', [UserController::class, 'show'])->name('usuarios.show');
+Route::delete('usuarios/{id}', [UserController::class, 'destroy'])->name('usuarios.destroy');
